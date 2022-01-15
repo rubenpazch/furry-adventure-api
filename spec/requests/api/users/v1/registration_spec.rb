f65b2264c0fad172a40a76575e52b170fbb158bd
ruben paz chuspe <rubenpazchuspe@outlook.com>
2022-01-15 07:48:16 -0500

    format code

--- spec/requests/api/users/v1/registration_spec.rb
+++ spec/requests/api/users/v1/registration_spec.rb
@@ -9,13 +9,13 @@ RSpec.describe "should create a new user", type: :request do
     it "should create a new user" do
       headers = { "ACCEPT" => "application/json" }
       post "/api/users/v1/registration", :params => {
-                                      :user => {
-                                        :email => @new_user.email,
-                                        :password => "P4ssw0rd",
-                                        :first_name => @new_user.first_name,
-                                        :last_name => @new_user.last_name,
-                                      },
-                                    }
+                                           :user => {
+                                             :email => @new_user.email,
+                                             :password => "P4ssw0rd",
+                                             :first_name => @new_user.first_name,
+                                             :last_name => @new_user.last_name,
+                                           },
+                                         }
       expect(response).to have_http_status(:created)
       json_response = JSON.parse(response.body)
       expect(json_response["username"]).not_to be_nil
@@ -26,17 +26,17 @@ RSpec.describe "should create a new user", type: :request do
       @duplicate_user.email = User.last.email
       headers = { "ACCEPT" => "application/json" }
       post "/api/users/v1/registration", params: {
-                                      :user => {
-                                        :email => @duplicate_user.email,
-                                        :password => "P4ssw0rd",
-                                        :first_name => @duplicate_user.first_name,
-                                        :last_name => @duplicate_user.last_name,
-                                      },
-                                    }
+                                           :user => {
+                                             :email => @duplicate_user.email,
+                                             :password => "P4ssw0rd",
+                                             :first_name => @duplicate_user.first_name,
+                                             :last_name => @duplicate_user.last_name,
+                                           },
+                                         }
       expect(response).to have_http_status(:unprocessable_entity)
       json_response = JSON.parse(response.body)
-      expect(json_response['errors'].first[1][0]).to eq('has already been taken')      
-      expect(json_response['errors'].first[0]).to eq('email')
+      expect(json_response["errors"].first[1][0]).to eq("has already been taken")
+      expect(json_response["errors"].first[0]).to eq("email")
     end
   end
 end
