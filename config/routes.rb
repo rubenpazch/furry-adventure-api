cb961d281dbf68509686ac0c45f9f4c4ede09568
ruben paz chuspe <rubenpazchuspe@outlook.com>
2022-01-22 07:03:52 -0500

    fix linter issues

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json} do
     namespace :v1 do
       resources :users, only: [:show, :create, :update, :destroy]
-      #resources :tokens, only: [:verify]
       resources :login, only: [:create]
       get 'tokens', to: 'tokens#verify'
       get 'users', to: 'users#admins'
