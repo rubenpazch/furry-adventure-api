cf5f05d75f33963672b727c7feb94e1467da2bb5
Ruben Paz Chuspe <ruben.paz@bairesdev.com>
2022-03-06 15:22:57 -0500

    upgrade rails version

--- Gemfile
+++ Gemfile
@@ -1,14 +1,14 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '3.0.1'
+ruby '3.1.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
-gem 'rails', '~> 6.1.4', '>= 6.1.4.1'
+gem 'rails', '~> 7.0.2', '>= 7.0.2.2'
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3', '~> 1.4'
 # Use Puma as the app server
-gem 'puma', '~> 5.0'
+gem "puma", "~> 5.0"
 
 gem 'pg'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
@@ -45,8 +45,10 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-gem "bcrypt", "~> 3.1"
+gem "bcrypt", "~> 3.1.7"
 
 gem "jwt", "~> 2.3"
 
 gem "pry", "~> 0.14.1"
+
+gem 'net-smtp', require: false
--- config/application.rb
+++ config/application.rb
@@ -1,19 +1,6 @@
 require_relative "boot"
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
-# require "sprockets/railtie"
-require "rails/test_unit/railtie"
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -30,7 +17,7 @@ module FurryAdventureApi
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    config.eager_load_paths << Rails.root.join("lib")
+    # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
@@ -38,4 +25,3 @@ module FurryAdventureApi
     config.api_only = true
   end
 end
-#Api::V1::UsersControllerTest#test_should_not_update_when_invalid_params_are_sent
