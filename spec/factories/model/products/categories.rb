44cb44a74d6655b3f37910dfd3424b593a4725b5
Ruben Paz Chuspe <ruben.paz@bairesdev.com>
2022-03-17 17:44:48 -0500

    add new category factory

--- spec/factories/model/products/categories.rb
+++ spec/factories/model/products/categories.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do 
-  factory :category, :class => Products::Category do 
-    title { Faker::Lorem.sentence(word_count: 3) }
+  category_name = Faker::Lorem.sentence(word_count: 3)
+
+  factory :products_category, :class => Products::Category do 
+    title { category_name }
     description { Faker::Lorem.sentence }
-    slug { Faker::Lorem.words(number: 3).join('-') }
+    slug { category_name.split(' ').join('-') }
     product_count { Faker::Number.between(from: 1, to: 100) }
     icon { Faker::LoremFlickr.image(size: "50x60") } 
   end
--- spec/requests/api/v1/products/categories_controller_spec.rb
+++ spec/requests/api/v1/products/categories_controller_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Products::Categories", type: :request do
+  before(:all) do 
+    @valid_category = create(:products_category)
+  end
+  
   context "GET /index" do
-    let(:valid_category)  { create :category }
-
     it "should return a list of categories" do 
       headers = { "ACCEPT" => "application/json"}
       get "/api/v1/products/categories"
       expect(response).to have_http_status(:ok)
+      json_response = JSON.parse(response.body)
+      expect(json_response.length).to be_equal(1)
     end
   end
 end
