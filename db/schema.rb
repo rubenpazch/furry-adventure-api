bd2c2aea68aa4aa79d0d3ca598728066ff039dbd
Ruben Paz Chuspe <ruben.paz@bairesdev.com>
2022-03-19 00:08:25 -0500

    add new table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_07_015049) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_19_031138) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -69,6 +69,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_07_015049) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "product_category_images", force: :cascade do |t|
+    t.string "thumbnail"
+    t.string "original"
+    t.bigint "product_categories_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_categories_id"], name: "index_product_category_images_on_product_categories_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.decimal "price"
@@ -109,5 +118,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_07_015049) do
   end
 
   add_foreign_key "jobs", "job_organizations"
+  add_foreign_key "product_category_images", "product_categories", column: "product_categories_id"
   add_foreign_key "products", "product_categories"
 end
