0f78bae85a6cdb13f85f6bc963e5c8d3e883adb4
Ruben Paz Chuspe <ruben.paz@bairesdev.com>
2022-03-20 20:19:38 -0500

    render json categories

--- app/controllers/api/v1/products/categories_controller.rb
+++ app/controllers/api/v1/products/categories_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::Products::CategoriesController < ApplicationController
   def index
     @categories = Products::Category.all
-    render jsonapi: @categories,
-           include: [:product_category_images],
+    render json: @categories.to_json(
+             :only => [:id, :title, :description, :slug, :product_count, :icon],
+             :include => {
+               :product_category_images => { only: [:thumbnail, :original, :product_categories_id] },
+             },
+           ),
            status: :ok
   end
 end
--- spec/requests/api/v1/products/categories_controller_spec.rb
+++ spec/requests/api/v1/products/categories_controller_spec.rb
@@ -4,11 +4,16 @@ RSpec.describe "Api::V1::Products::Categories", type: :request do
   context "GET /index" do
     let(:valid_category) { build(:products_category) }
     let(:valid_category_image) { build(:product_category_images) }
+    let(:valid_category_two) { build(:products_category) }
+    let(:valid_category_image_two) { build(:product_category_images) }
 
     before(:each) do
       valid_category.save!
       valid_category_image.product_categories_id = valid_category.id
       valid_category_image.save!
+      valid_category_two.save!
+      valid_category_image_two.product_categories_id = valid_category_two.id
+      valid_category_image_two.save!
     end
 
     it "should return a list of categories" do
@@ -16,7 +21,8 @@ RSpec.describe "Api::V1::Products::Categories", type: :request do
       get "/api/v1/products/categories", headers: headers
       expect(response).to have_http_status(:ok)
       json_response = JSON.parse(response.body)
-      expect(json_response["data"].length).to be_equal(1)
+      binding.pry
+      expect(json_response["data"].length).to be_equal(2)
     end
   end
 end
