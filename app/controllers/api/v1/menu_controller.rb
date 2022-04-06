03f7aa3b75a571fcdb11d91e6cab710082dd4d0a
Ruben Paz Chuspe <ruben.paz@bairesdev.com>
2022-04-06 02:26:24 -0500

    secutiry adds

--- app/controllers/api/v1/menu_controller.rb
+++ app/controllers/api/v1/menu_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::MenuController < ApplicationController
   before_action :check_login, only: %i[index]
 
   def menusubmenu
-    @menu = Profiles::Menu.where(account_id: current_user.account_id)
+    @menu = current_user.is_super_admin? ? Profiles::Menu.all : current_user.role.profile.menus
     render json: @menu.to_json(
              :only => [:id, :title, :link_to, :hasSubMenu, :isRoot, :account_id],
              :include => {
