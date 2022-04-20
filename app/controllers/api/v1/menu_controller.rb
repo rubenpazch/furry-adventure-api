039d9f61c98d9e5955a64c86a898f9c631ec0614
Ruben Paz Chuspe <ruben.paz@bairesdev.com>
2022-04-20 00:59:12 -0500

    menu

--- app/controllers/api/v1/menu_controller.rb
+++ app/controllers/api/v1/menu_controller.rb
@@ -1,14 +1,9 @@
 class Api::V1::MenuController < ApplicationController
   before_action :check_login, only: %i[index]
 
-  def menusubmenu
+  def menu_submenu
     @menu = current_user.is_super_admin? ? Profiles::Menu.all : current_user.role.profile.menus
-    render json: @menu.to_json(
-             :only => [:id, :title, :link_to, :hasSubMenu, :isRoot, :account_id],
-             :include => {
-               :sub_menus => { only: [:title, :link_to, :hasSubMenu, :menus_id] },
-             },
-           ),
+    render json: Profiles::MenuSerializer.new(@menu),
            status: :ok
   end
 end
---
+++ app/serializers/profiles/menu_serializer.rb
@@ -0,0 +1,7 @@
+class Profiles::MenuSerializer
+  include JSONAPI::Serializer
+  attributes :id, :title, :link_to, :hasSubMenu, :isRoot, :account_id, :sub_menus
+  has_many :sub_menus, class_name: "Profiles::SubMenu", foreign_key: "menus_id"
+  #belongs_to :account, class_name: "Account"
+  #has_many :menu_accesses, class_name: "Profiles::MenuAccess", foreign_key: "menus_id"
+end
