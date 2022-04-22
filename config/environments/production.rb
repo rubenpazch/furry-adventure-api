b3a4f71fb7af69e31da34b7dd127c0fe6ed5fd6a
Ruben Paz Chuspe <ruben.paz@bairesdev.com>
2022-04-22 17:10:08 -0500

    Update production cors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.allowed_cors_origins = ["https://web-react-sulca.herokuapp.com/"]
+
 end
